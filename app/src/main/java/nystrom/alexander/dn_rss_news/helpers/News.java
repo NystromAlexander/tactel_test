006f729b3de2a6f75b259398ee708b9cf0494237
Alexander Nyström <dv15anm@cs.umu.se>
2018-04-27 10:20:40 +0200

    news model class

---
+++ app/src/main/java/nystrom/alexander/dn_rss_news/helpers/News.java
@@ -0,0 +1,39 @@
+package nystrom.alexander.dn_rss_news.helpers;
+
+import java.util.Date;
+
+/**
+ * Created by dv15anm on 2018-04-27.
+ *
+ * Class to represent a news instance
+ */
+public class News {
+    private String mTitle;
+    private String mNewsUrl;
+    private String mDescritption;
+    private Date mPublished;
+
+    public News(String title, String url, String descritption, Date published) {
+        mTitle = title;
+        mNewsUrl = url;
+        mDescritption = descritption;
+        mPublished = published;
+    }
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getNewsUrl() {
+        return mNewsUrl;
+    }
+
+    public String getDescritption() {
+        return mDescritption;
+    }
+
+    public Date getPublished() {
+        return mPublished;
+    }
+}
